fc305d5f4d948fbef8d4283796d486e64544e8ff
pj67331n <pj67331n@pace.edu>
2019-08-30 16:01:36 -0400

    Angular first commit

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  search(name: string): void {
+    this.heroService.searchHeroes(name).subscribe(
+      prat => { this.heroes = prat }
+    );
+  }
+}
